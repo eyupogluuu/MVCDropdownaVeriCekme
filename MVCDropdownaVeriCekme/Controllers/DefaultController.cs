560b0dea6fe0ecdd3ada12a85fa7217014b7e7b4
casper <casper@DESKTOP-2PU1K7L>
2022-08-03 21:49:11 +0300

    Add project files.

---
+++ MVCDropdownaVeriCekme/Controllers/DefaultController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDropdownaVeriCekme.Models.Entity;
+
+namespace MVCDropdownaVeriCekme.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        Entities db = new Entities();
+        public ActionResult Index()
+        {
+            List<SelectListItem> sehirler = (from i in db.tbl_sehirler.ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.Sehir,
+                                                 Value = i.ID.ToString()
+                                             }).ToList();
+            ViewBag.shr = sehirler;
+            return View();
+        }
+    }
+}
---
+++ MVCDropdownaVeriCekme/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCDropdownaVeriCekme.MvcApplication" Language="C#" %>
